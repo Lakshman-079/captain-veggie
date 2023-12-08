4cf1f332e3c35282a9dd53ce2dc13a979cb20cc5
hramakur <150870576+hramakur@users.noreply.github.com>
2023-12-08 18:43:19 -0500

    Add files via upload

---
+++ Rabbit.py
@@ -0,0 +1,12 @@
+
+# Author Name: Talluri Lakshman Sai, Naramreddy Manoj Sai, Haveela E Joycy Ramakuri
+# Date: 12/07/2023
+# Description: Class for Rabbit that is a subclass of Creature
+
+from Creature import Creature
+
+
+class Rabbit(Creature):
+    def __init__(self, x, y):
+        super().__init__(x, y, "R")
+
